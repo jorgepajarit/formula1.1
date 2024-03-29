06b542395d153b91c11a88d4db4bd28568206c11
Juancasta14 <juanfelipe9902@gmail.com>
2024-03-28 20:04:57 -0500

    Create circuitos.js

---
+++ js/circuitos.js
@@ -0,0 +1,35 @@
+
+function pausarYReiniciarVideo() {
+    // Pausar el video
+    var videoIframe = document.querySelector('.video-circuito iframe');
+    videoIframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+    
+    // Reiniciar el video al principio
+    videoIframe.contentWindow.postMessage('{"event":"command","func":"seekTo","args":[0,true]}', '*');
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const circuitos = document.querySelectorAll('#circuitos .circuitos-lista li');
+    circuitos.forEach((circuito, index) => {
+      circuito.addEventListener('click', () => {
+        document.querySelectorAll('.circuitos-info').forEach(info => {
+          info.style.display = 'none';
+        });
+        const info = circuito.querySelector('.circuitos-info');
+        const cerrar = info.querySelector('.cerrar');
+
+        if (info) {
+          info.style.display = 'block';
+        }
+        cerrar.addEventListener('click', (event) => {
+          event.stopPropagation(); // Evitar que se cierre al hacer clic en el botón de cerrar
+          pausarYReiniciarVideo();
+          info.style.display = 'none';
+          
+        
+        });
+      });
+    });
+  });
+
